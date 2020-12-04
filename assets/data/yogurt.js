8b4b7c5899e93a7e1a2c7f32b04d0cb9104f7f52
Julio Canizalez <juliocanizalez@outlook.com>
2020-12-03 18:14:35 -0600

    rearranging the project logic

---
+++ assets/data/yogurt.js
@@ -0,0 +1,59 @@
+let yogurt = [ 
+    {   
+        "name": "Yogurt Dos Pinos",
+        "url": "../assets/img/productos/ydospinos.jpg",
+        "price": 2.54        
+    },
+    {
+        "name": "Yogurt de Fresa",
+        "url": "../assets/img/productos/yyes.webp",
+        "price": 1.49
+        
+    },
+    {
+        "name": "Yogurt de Frutas",
+        "url": "../assets/img/productos/yfrutas.jpg",
+        "price": 1.45   
+    },
+    {
+        "name": "Yogurt de Melocotón",
+        "url": "../assets/img/productos/ymelocoton.webp",
+        "price": 1.78   
+    },
+    {
+        "name": "Yogurt de Arándanos",
+        "url": "../assets/img/productos/yarandano.webp",
+        "price": 1.49    
+        
+    },
+    {
+        "name": "Yogurt de Uva",
+        "url": "../assets/img/productos/yuva.jpg",
+        "price": 1.59
+    },
+    {
+        "name": "Yogurt de Manzana",
+        "url": "../assets/img/productos/ymanzana.webp",
+        "price": 1.49        
+    },
+    {
+        "name": "Yogurt Banano Fresa",
+        "url": "../assets/img/productos/ybananofresa.jpg",
+        "price": 1
+    },
+    {
+        "id":9,
+        "name": "4 Pack Yogurt Líquido YES",
+        "url": "../assets/img/productos/ypack4.jpg",
+        "price": 1.99      
+        
+    },
+    {
+        "id":10,
+        "name": "12 Pack Yogurt Líquido COLUN",
+        "url": "../assets/img/productos/ypack12.jpeg",
+        "price": 2.49         
+    }
+]
+
+export default yogurt
